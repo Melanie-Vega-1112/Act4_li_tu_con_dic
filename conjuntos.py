5ba44ce71573f809396cacb0e7c7c5b7517bdc1a
Melanie-Vega-1112 <164534346+Melanie-Vega-1112@users.noreply.github.com>
2024-09-06 12:30:05 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,4 @@
+alumno = {"rosa", "verde", "morado"}
+print(alumno)
+for colores in alumno:
+    print(colores)
---
+++ diccionario.py
@@ -0,0 +1,6 @@
+thisdict = {
+  "Telefono": "iphone",
+  "modelo": "16",
+  "year": 2024
+}
+print(thisdict)
